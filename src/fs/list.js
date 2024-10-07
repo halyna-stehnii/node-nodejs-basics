cf02c317cbf1dcc0d200f3b96cf2b639648be33c
Halyna Stehnii <galyna.stegniy@gmail.com>
2024-10-08 00:11:46 +0200

    feat: list task solved

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,20 @@
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import { readdir } from 'fs/promises';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const list = async () => {
-    // Write your code here 
+  const path = __dirname + '/files';
+  const errorMsg = 'FS operation failed';
+
+  try {
+    const allFiles = await readdir(path);
+    allFiles.forEach((file) => console.log(file));
+  } catch (err) {
+    throw new Error(errorMsg);
+  }
 };
 
 await list();
