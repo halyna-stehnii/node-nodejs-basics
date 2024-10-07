56d692346bf3e017cc94fdb4e295f2219b18c80d
Halyna Stehnii <galyna.stegniy@gmail.com>
2024-10-08 00:43:13 +0200

    feat: hash task solved

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,28 @@
+import fs from 'fs';
+import { dirname } from 'path';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import crypto from 'crypto';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const filePath = path.join(__dirname, 'files', 'fileToCalculateHashFor.txt');
+const fileStream = fs.createReadStream(filePath);
+const hash = crypto.createHash('sha256');
+
 const calculateHash = async () => {
-    // Write your code here 
+  fileStream.on('data', (chunk) => {
+    hash.update(chunk);
+  });
+
+  fileStream.on('end', () => {
+    const result = hash.digest('hex');
+    console.log(`SHA256 hash: ${result}`);
+  });
+
+  fileStream.on('error', (err) => {
+    console.error(`Error reading file: ${err.message}`);
+  });
 };
 
 await calculateHash();
