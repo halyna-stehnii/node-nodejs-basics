38f16746673afce25c18cb75b8d4eb48e8b1342a
Halyna Stehnii <galyna.stegniy@gmail.com>
2024-10-08 00:09:11 +0200

    feat: delete task solved

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,18 @@
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import { unlink } from 'fs/promises';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const remove = async () => {
-    // Write your code here 
+  const path = __dirname + '/files/fileToRemove.txt';
+  const errorMsg = 'FS operation failed';
+  try {
+    await unlink(path);
+  } catch (err) {
+    throw new Error(errorMsg);
+  }
 };
 
 await remove();
