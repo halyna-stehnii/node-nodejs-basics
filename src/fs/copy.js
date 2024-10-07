271bd79a3bc98f2b965267f69e6b805e3c2b986f
Halyna Stehnii <galyna.stegniy@gmail.com>
2024-10-08 00:06:45 +0200

    feat: copy task solved

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,26 @@
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import { copyFile, mkdir, readdir } from 'fs/promises';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const copy = async () => {
-    // Write your code here 
+  const srcFolder = __dirname + '/files';
+  const destFolder = __dirname + '/files_copy';
+  const errorMsg = 'FS operation failed';
+
+  try {
+    await mkdir(destFolder);
+    const tmp = await readdir(srcFolder);
+    await Promise.all(
+      tmp.map((file) =>
+        copyFile(`${srcFolder}/${file}`, `${destFolder}/${file}`)
+      )
+    );
+  } catch (err) {
+    throw new Error(errorMsg);
+  }
 };
 
 await copy();
