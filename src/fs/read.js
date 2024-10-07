8a4047641d2cfd53034f387674b97181d5d0deac
Halyna Stehnii <galyna.stegniy@gmail.com>
2024-10-08 00:12:51 +0200

    feat: read task solved

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,20 @@
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import { readFile } from 'fs/promises';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const read = async () => {
-    // Write your code here 
+  const path = __dirname + '/files/fileToRead.txt';
+  const errorMsg = 'FS operation failed';
+
+  try {
+    const content = await readFile(path, 'utf8');
+    console.log(content);
+  } catch (err) {
+    throw new Error(errorMsg);
+  }
 };
 
 await read();
