2cb39b37a73baf9c6f9b1b7551096f073e2f587c
Halyna Stehnii <galyna.stegniy@gmail.com>
2024-10-08 00:01:02 +0200

    feat: env task solved

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,11 @@
 const parseEnv = () => {
-    // Write your code here 
+  const rssVariables = Object.keys(process.env)
+    .filter((key) => key.startsWith('RSS_'))
+    .map((key) => `${key}=${process.env[key]}`)
+    .join('; ');
+
+  console.log(rssVariables);
 };
+//RSS_name1=value1 RSS_name2=value2 node cli/env.js
 
 parseEnv();
