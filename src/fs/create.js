ae92442d17ef17bc96dfae790eeabf067f53cf28
Halyna Stehnii <galyna.stegniy@gmail.com>
2024-10-08 00:08:04 +0200

    feat: create task solved

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,20 @@
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import { writeFile } from 'fs/promises';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const create = async () => {
-    // Write your code here 
+  const path = __dirname + '/files/fresh.txt';
+  const errorMsg = 'FS operation failed';
+  const fileContent = 'I am fresh and young';
+
+  try {
+    await writeFile(path, fileContent, { flag: 'wx' });
+  } catch (err) {
+    throw new Error(errorMsg);
+  }
 };
 
 await create();
