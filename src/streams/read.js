5c6d78373bfc361eefa1da83820a02cfd5f1ab5c
Halyna Stehnii <galyna.stegniy@gmail.com>
2024-10-08 11:11:33 +0200

    feat: streams-read task solved

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,15 @@
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { pipeline } from 'stream/promises';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const filePath = path.join(__dirname, 'files', 'fileToRead.txt');
+const readStream = fs.createReadStream(filePath);
+
 const read = async () => {
-    // Write your code here 
+  await pipeline(readStream, process.stdout);
 };
 
 await read();
