1e915d8d99992ab7bd6634f407a1e95526dc2906
Halyna Stehnii <galyna.stegniy@gmail.com>
2025-10-26 21:33:31 +0100

    feat: add rename

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,18 @@
+import { getDirname } from "./fileUtils.js";
+import { rename as changeName } from "fs/promises";
+
+const __dirname = getDirname(import.meta.url);
+
 const rename = async () => {
-  // Write your code here
+  const properName = __dirname + "/files/properFilename.md";
+  const wrongName = __dirname + "/files/wrongFilename.txt";
+  const errorMsg = "FS operation failed";
+
+  try {
+    await changeName(wrongName, properName);
+  } catch (err) {
+    throw new Error(errorMsg);
+  }
 };
 
 await rename();
