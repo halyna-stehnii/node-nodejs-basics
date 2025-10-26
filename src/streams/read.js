2567e25e0dba59811e21c28e28ba491b0e1fa19f
Halyna Stehnii <galyna.stegniy@gmail.com>
2025-10-26 22:41:27 +0100

    feat: add read

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,13 @@
+import fs from "fs";
+import path from "path";
+import { getDirname } from "../utils/fileUtils.js";
+
+const __dirname = getDirname(import.meta.url);
+const filePath = path.join(__dirname, "files", "fileToRead.txt");
+
 const read = async () => {
-  // Write your code here
+  const readStream = fs.createReadStream(filePath, "utf8");
+  readStream.pipe(process.stdout);
 };
 
 await read();
